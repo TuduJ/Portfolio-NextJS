190c5e94be72dafa2fa7eca8ee2eaf9c56ebb816
Jitendranath Tudu <tudu_j@Jitendranaths-MacBook-Pro.local>
2023-07-26 21:44:17 +0530

    dependencies installed

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,29 @@
-import '@/styles/globals.css'
+import Navbar from "@/components/Navbar";
+import "@/styles/globals.css";
+import {Montserrat, Raleway} from 'next/font/google'
+import Head from "next/head";
+
+const montserrat = Montserrat({
+  subsets: ["latin"],
+  variable: "--font-mont",
+})
+
+const raleway = Raleway({
+  subsets: ["latin"],
+  variable: "--font-raleway",
+})
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main  className={`${raleway.variable} font-raleway bg-light w-full min-h-screen`}>
+        <Navbar/>
+        <Component {...pageProps} />
+      </main>
+    </>
+  );
 }
